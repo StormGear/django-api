5e610253e4a9c47cb600118ee3aabc0d02e9c421
Papa Kofi Boahen <papakofiboahen@gmail.com>
2024-11-27 16:34:43 +0000

    first db migration

--- users/models.py
+++ users/models.py
@@ -2,8 +2,14 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True) 
     email = models.EmailField(max_length=100)
     
     def __str__(self):
         return self.name
+    
+    class Meta:
+        ordering = ['id',]
+        db_table = 'users'
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
