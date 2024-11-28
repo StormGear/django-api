17fac9f2a2bbe810d905f925a7d0e1847ffd1037
Papa Kofi Boahen <papakofiboahen@gmail.com>
2024-11-28 19:57:49 +0000

    secret key added

--- simple_django_proj/settings.py
+++ simple_django_proj/settings.py
@@ -133,7 +133,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://sub.example.com",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
-    "https://simple-django-proj-6d2134b75d76.herokuapp.com:8000"
+    "https://simple-django-proj-6d2134b75d76.herokuapp.com"
 ]
 
 # Optionally, allow all origins (not recommended for production)
