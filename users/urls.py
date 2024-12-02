0c2d0b9fc37c27a69cf1a4f304e2225fd7fccd83
Papa Kofi Boahen <papakofiboahen@gmail.com>
2024-12-02 23:52:05 +0000

    swagger docs

--- users/urls.py
+++ users/urls.py
@@ -5,9 +5,9 @@ from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = [
     # path('', views.users_list),
     # path('<int:pk>', views.user_detail),
-    path('', views.AddUsers.as_view()),
+    path('add-users/', views.AddUsers.as_view()),
     path('<int:pk>/', views.UserDetail.as_view()),
-    path('add-users/', views.UserList.as_view()),
+    path('get-users/', views.UserList.as_view()),
     path('update-users/<int:pk>/', views.UserUpdate.as_view()),
     path('delete-users/<int:pk>/', views.UserDelete.as_view()),
 ]
